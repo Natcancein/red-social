ce7f20f173a54c20474edcd32e8dd0dab1ba6e2a
Natcancein <nataliacancino@MacBook-Pro-de-Natalia.local>
2018-01-03 13:03:38 -0300

    actualización archivo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();   
 });
+
+$("#btn").click(function saveCom(){
+    var comment = $("#comment").val();
+    
+    $("#post").append( "<div class='posteo'>"+"<img src='assets/img/contacts/animalista10.jpg' class='circle-min'>" + "<h5 class='lado-nombre'>Natalia Arancibia</h5>" +
+comment + "</br>" +"</div>");
+    
+});
+
+
+
+
