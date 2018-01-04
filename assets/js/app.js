6e844d6e76383f93f1cdde5d47b4d8fb32ed2733
Natcancein <nataliacancino@MacBook-Pro-de-Natalia.local>
2018-01-04 10:06:25 -0300

    actualizacion subir fotos

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,41 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();   
 });
 
+$('#preview').hover(
+    function() {
+        $(this).find('a').fadeIn();
+    }, function() {
+        $(this).find('a').fadeOut();
+    }
+)
+$('#file-select').on('click', function(e) {
+     e.preventDefault();
+    
+    $('#file').click();
+})
+
+
+$('#file').hide();
+
+$('input[type=file]').change(function() {
+    var file = (this.files[0].name).toString();
+    var reader = new FileReader();
+    
+    $('#file-info').text('');
+    $('#file-info').text(file);
+    
+     reader.onload = function (e) {
+         $('#preview img').attr('src', e.target.result);
+	 }
+     
+     reader.readAsDataURL(this.files[0]);
+});
+
+$("#btn2").click(function publicar(){
+	var imagen = $("#img-guardar");
+ $("#post-img").append("<div class='posteo'>"+"<img src='assets/img/contacts/perfil.jpg' class='circle-min'>" + "<h5 class='lado-nombre'>Natalia Arancibia</h5>" + "<img src="+imagen[0].src+">" );
+});
+
 $("#btn").click(function saveCom(){
     var comment = $("#comment").val();
     
@@ -9,6 +44,8 @@ $("#btn").click(function saveCom(){
 comment + "</br>" +"</div>");
     
 });
+
+$("#file").hide();
 
 $("#seguir").mouseenter(function dejar(){
 	$("#seguir").text("dejar de seguir").css("font-size","10px");
